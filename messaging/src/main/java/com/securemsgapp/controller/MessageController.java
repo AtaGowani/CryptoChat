3c9127bcd8191293a1a56c33d97846cfa023ff3c
Alexis Vu <vu@utdallas.edu>
2020-10-16 02:44:43 -0500

    add REST API controller file

---
+++ messaging/src/main/java/com/securemsgapp/controller/MessageController.java
@@ -0,0 +1,9 @@
+package com.securemsgapp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping(value = "/msg-api")
+public class MessageController {
+}
