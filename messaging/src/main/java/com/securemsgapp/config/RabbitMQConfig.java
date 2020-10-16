296cf1cd713266b2c4677e7b5d469466115b0bb3
Alexis Vu <vu@utdallas.edu>
2020-10-16 18:15:04 -0500

    add MessageListenerContainer to receive messages from message queue

--- messaging/src/main/java/com/securemsgapp/config/RabbitMQConfig.java
+++ messaging/src/main/java/com/securemsgapp/config/RabbitMQConfig.java
@@ -7,11 +7,15 @@ import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.securemsgapp.service.RabbitMQListener;
 
 @Configuration
 public class RabbitMQConfig {
@@ -50,5 +54,14 @@ public class RabbitMQConfig {
         final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(jsonMessageConverter());
         return rabbitTemplate;
+    }
+
+    @Bean
+    MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory) {
+        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+        simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
+        simpleMessageListenerContainer.setQueues(queue());
+        simpleMessageListenerContainer.setMessageListener(new RabbitMQListener());
+        return simpleMessageListenerContainer;
     }
 }
