f4dde29eb4898cbf7aaa5418bb2d3c02bb808c26
Alexis Vu <vualexis@outlook.com>
2020-10-15 19:46:32 -0500

    Set up basic Java Spring Boot backend for message handling API (#1)
    Reasons for PR:
    * need messaging backend that is separate from rest of project backend to decouple key management
    * decoupled key management will be important for later implementations of end-to-end encryption
    
    Reasons for using Java Spring Boot:
    * has support for many important libraries and modules that will be needed later for message queuing and endpoint authentication
    * Java is work package partner's language of choice

---
+++ messaging/src/main/java/com/securemsgapp/securemsgapp/SecureMsgAppApplication.java
@@ -0,0 +1,13 @@
+package com.securemsgapp.securemsgapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureMsgAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureMsgAppApplication.class, args);
+	}
+
+}
---
+++ messaging/src/test/java/com/securemsgapp/securemsgapp/SecureMsgAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securemsgapp.securemsgapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureMsgAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
