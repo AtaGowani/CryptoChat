fa86122bfc54c08dd7ebbf3c0e467ab9f0ba1a1c
Alexis Vu <vu@utdallas.edu>
2020-10-16 04:51:49 -0500

    on send API call, send message to RabbitMQ queue

--- messaging/src/main/java/com/securemsgapp/controller/MessageController.java
+++ messaging/src/main/java/com/securemsgapp/controller/MessageController.java
@@ -1,9 +1,26 @@
 package com.securemsgapp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.securemsgapp.model.Message;
+import com.securemsgapp.service.RabbitMQSender;
 
 @RestController
 @RequestMapping(value = "/msg-api")
 public class MessageController {
+    @Autowired
+    RabbitMQSender rabbitMQSender;
+
+    @PostMapping(value = "/send")
+    public String sendMessage() {
+        Message message = new Message();
+        message.setBody("hi");
+        rabbitMQSender.send(message);
+
+        return "Message sent";
+    }
 }
