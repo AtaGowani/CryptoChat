68855dbf3d3ba03e03860271d45d38b77c121cf6
Ata Gowani <atagowani@gmail.com>
2020-10-29 01:00:25 -0500

    Seperate db module

---
+++ backend/database/__init__.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+
+def init_db(app):
+  db = SQLAlchemy(app)
+
+  class User(db.Model):
+      __tablename__ = 'user'
+      id = db.Column(db.Integer, primary_key=True)
+      email = db.Column(db.VARCHAR(100))
+      password = db.Column(db.Text())
+      phone = db.Column(db.CHAR(10))
+      public_key = db.Column(db.Text())
+
+      def __init__(self, email, password, phone, public_key):
+          self.email = email
+          self.password = password
+          self.phone = phone
+          self.public_key = public_key
+
+  return db
