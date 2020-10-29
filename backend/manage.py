060ea47f6c565c44884a1750982d10ea559cfd77
Ata Gowani <atagowani@gmail.com>
2020-10-29 00:36:42 -0500

    Add command to init db

--- backend/manage.py
+++ backend/manage.py
@@ -1,5 +1,6 @@
 import click
 from app import create_app
+from database import init_db
 # Import dependencies here
 # For example, if you were using SQLAlchem
 # from app import create_app, db
@@ -16,26 +17,30 @@ def cli():
     help='Environment to use while running server',
     type=click.STRING
 )
+
+def initdb(env):
+    app = create_app(env)
+    db = init_db(app)
+    db.create_all()
+
+@click.command()
+@click.option(
+    '--env', default='development',
+    help='Environment to use while running server',
+    type=click.STRING
+)
 @click.option(
     '--port', default=5000,
     help='Port to use while running server',
     type=click.STRING
 )
+
 def runserver(env, port):
     app = create_app(env)
     app.run(port=port)
 
-# Add other commands here
-# Eg.
-# def initdb():
-#    ''' Create the SQL DB '''
-#    db.create_all()
-
-
-cli.add_command(runserver)
-# Register other commands here
-# Eg.
-# cli.add_command(initdb)
+cli.add_command(cmd=runserver, name="runserver")
+cli.add_command(cmd=initdb, name="initdb")
 
 if __name__ == "__main__":
     cli()
