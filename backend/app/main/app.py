e1d0d8025f7336c4ef17f998b37fe12c338e037d
Ata Gowani <atagowani@gmail.com>
2020-10-29 22:40:32 -0500

    Add template functions to test

--- backend/app/main/app.py
+++ backend/app/main/app.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, send_from_directory
+from flask import Blueprint, request
 from ..models.User import db, User
 import os
 
@@ -9,18 +9,39 @@ main = Blueprint(
     url_prefix='/'
 )
 
+def verify_pass(email, pw):
+    user = User.query.filter(User.email == email).first().__dict__
+    if (user["password"] == pw):
+        return True
+    return False
+
 @main.route('/', methods=['GET'])
 def index():
     return "Welcome to the Backend"
 
 @main.route('/signup', methods=['POST'])
 def signup():
+    email = request.form.get("email").upper()
+    password = request.form.get("password")
+    phone = request.form.get("phone")
+    public_key = "SAMPLE_PUBLIC_KEY"
+
     # Example of how data can be created initialized db
-    # data = User("test@email.com", "password", "1234235465", "lhauiaehhhluhs7832yhek")
-    # db.session.add(data)
-    # db.session.commit()
+    data = User(email, password, phone, public_key)
+    db.session.add(data)
+    db.session.commit()
+    
     return {"ok": 200}
 
 @main.route('/signin', methods=['POST'])
 def signin():
-    return None
+    print("Line  38")
+    email = request.form.get("email").upper()
+    user = User.query.filter(User.email == email).first()
+    print("Line  41")
+    if user:
+        user = user.__dict__
+        if verify_pass(email, request.form.get("password")):
+            return {"message": "user found"}
+    print("Line  46")
+    return {"message": "user NOT found"}
