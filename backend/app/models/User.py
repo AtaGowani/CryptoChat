37c0a20c8c299aac9f7b9ae05910093e491d18a4
Ata Gowani <atagowani@gmail.com>
2020-11-03 00:08:53 -0600

    Update db schema

--- backend/app/models/User.py
+++ backend/app/models/User.py
@@ -4,11 +4,11 @@ db = SQLAlchemy()
 
 class User(db.Model):
   __tablename__ = 'user'
-  id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
-  email = db.Column(db.VARCHAR(100), unique=True)
-  phone = db.Column(db.CHAR(10), unique=True)
-  password = db.Column(db.Text())
-  public_key = db.Column(db.Text())
+  id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
+  email = db.Column(db.VARCHAR(100), unique=True, nullable=False)
+  phone = db.Column(db.CHAR(11), unique=True, nullable=False)
+  password = db.Column(db.Text(), nullable=False)
+  public_key = db.Column(db.Text(), unique=True, nullable=False)
 
   def __init__(self, email, password, phone, public_key):
     self.email = email
