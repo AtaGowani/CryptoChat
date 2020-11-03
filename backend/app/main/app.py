9174fe2a318f1966ed381a0eed2c9b3ba864ec71
Ata Gowani <atagowani@gmail.com>
2020-11-02 19:38:49 -0600

    Add proper API json response

--- backend/app/main/app.py
+++ backend/app/main/app.py
@@ -1,6 +1,7 @@
 import hashlib
+import json
 
-from flask import Blueprint, request
+from flask import Blueprint, request, Response
 from ..models.User import db, User
 
 main = Blueprint(
@@ -13,16 +14,18 @@ main = Blueprint(
 def create_user(email, password, phone, public_key):
     user_email = User.query.filter(User.email == email).first()
     user_phone = User.query.filter(User.phone == phone).first()
+    user_pk = User.query.filter(User.public_key == public_key).first()
 
-    if user_email or user_phone:
-        print("USER NOT CREATED. PHONE OR EMAIL ALREADY EXISTS.")
-        return
+    if user_email or user_phone or user_pk:
+        print("USER NOT CREATED. PHONE, EMAIL OR PK ALREADY EXISTS.")
+        return False
     
     password = hashlib.sha256(password.encode()).hexdigest()
     email = email.upper()
     data = User(email, password, phone, public_key)
     db.session.add(data)
     db.session.commit()
+    return data
 
 def verify_pass(email, pw):
     user = User.query.filter(User.email == email).first().__dict__
@@ -37,14 +40,26 @@ def index():
 @main.route('/signup', methods=['POST'])
 def signup():
     # TO-DO: IMPLEMENT PROPER SIGNUP API
-    email = request.form.get("email").upper()
+    email = request.form.get("email").upper().strip()
     password = request.form.get("password")
-    phone = request.form.get("phone")
-    public_key = request.form.get("pk")
-
-    create_user(email, password, phone, public_key)
+    phone = request.form.get("phone").strip()
+    public_key = request.form.get("pk").strip()
     
-    return {"ok": 200}
+    if not email or not password or not phone or not public_key:
+        return Response('{"message": "missing required fields"}', status=400, mimetype='application/json')
+
+    user = create_user(email, password, phone, public_key)
+    
+    user = {
+        "email": user.email,
+        "phone": user.phone,
+        "pk": user.public_key
+    }
+
+    if not user:
+        return Response('{"message": "phone number, email or pk already exists"}', status=400, mimetype='application/json')
+    
+    return Response(json.dumps(user), status=200, mimetype='application/json')
 
 @main.route('/signin', methods=['POST'])
 def signin():
