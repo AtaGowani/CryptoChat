fbab774a36ab64203231d76dd7dd424ceada44a2
Alexis Vu <vu@utdallas.edu>
2020-11-08 15:57:30 -0600

    remove unused code

--- messaging/src/main/java/com/securemsgapp/model/MessageManager.java
+++ messaging/src/main/java/com/securemsgapp/model/MessageManager.java
@@ -2,7 +2,6 @@ package com.securemsgapp.model;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 
 import java.util.ArrayList;
 import java.util.HashMap;
